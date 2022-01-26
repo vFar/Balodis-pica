0c5330d0b1da014cb65924fadbb92e80209642a0
Balodis <MarkussDove@gmail.com>
2022-01-26 11:30:27 +0200

    Papildināta case pasutit un pievienoti boolean vaicajumi piegadei un dzerienam

--- src/Pica.java
+++ src/Pica.java
@@ -1,14 +1,15 @@
 import javax.swing.JOptionPane;
 
 public class Pica {
-	static pasutit() {
-		return 0;
+	static double Margherita(double cena) {
+		
+		return cena;
 	}
 	public static void main(String[] args) {
-		int cena; 
+		double cena=0;
 		String izvele;
-		String adrese, vards, talrunis;
-		boolean piegade, dzeriens;
+		String adrese="", vards, talrunis, veids, piegade1, dzeriens1;
+		boolean piegade=false, dzeriens=false;
 		
 		do {
 			izvele = JOptionPane.showInputDialog("pasutit - veikt pasūtījumu "
@@ -18,11 +19,32 @@ public class Pica {
 			
 			switch(izvele) {
 			case "pasutit":
-				String veids = JOptionPane.showInputDialog("Kādu picas veidu izvēlēsies? "
+				veids = JOptionPane.showInputDialog("Kādu picas veidu izvēlēsies? "
 						+ "\nMargherita | Pepperoni | Pikantā | Kalifornija ");
 				
+				dzeriens1 = JOptionPane.showInputDialog("Piedāvāt dzērienu no mūsu kolekcijas? "
+						+ "\nCoca-Cola +0.80centi || Sprite +0.60centi || Fanta +0.80centi || Negāzēts ūdens +0.30centi || Atstāj tukšu, ja nevēlies dzērienu");
+				if(dzeriens1.equalsIgnoreCase("Coca-Cola") || dzeriens1.equalsIgnoreCase("Sprite") || dzeriens1.equalsIgnoreCase("Fanta") || dzeriens1.equalsIgnoreCase("Negāzēts ūdens")) {
+					dzeriens=true;
+				}else {
+					dzeriens=false;
+				}
+				System.out.println(dzeriens);
+				
+				piegade1 = JOptionPane.showInputDialog("Piegāde uz mājām, jā vai nē?");
+				if(piegade1.equalsIgnoreCase("jā")) {
+					piegade = true;
+					adrese = JOptionPane.showInputDialog("Uz kuru adresi piegādāt picu?");
+					talrunis = JOptionPane.showInputDialog("Kāds Jums talrunis uz, kuru zvanīt?");
+				}else if(piegade1.equalsIgnoreCase("nē")) {
+					piegade = false;
+				}else {
+					JOptionPane.showMessageDialog(null, "Vajadzēja ievadīt tikai jā vai nē!");
+				}
+				
+				
 				if(veids.equalsIgnoreCase("Margherita")) {
-					JOptionPane.showMessageDialog(null, veids);
+					Margherita(cena);
 				}else if(veids.equalsIgnoreCase("Pepperoni")) {
 					
 				}else if(veids.equalsIgnoreCase("Pikantā")) {
